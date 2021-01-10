4d7e46c3bd989ddd765c33c467f37def1253246a
Efosa Osazuwa <efosazuwa0@gmail.com>
2021-01-10 13:45:44 -0700

    creates the rare label encoder class

--- preprocesspipeline.py
+++ preprocesspipeline.py
@@ -64,3 +64,31 @@ class TemporalVariableEstimator(BaseEstimator, TransformerMixin):
             X[feature] = X[self.reference_variables] - X[feature]
 
         return X
+
+#Frequent label categorical encoder
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, tol=0.05, variables=None):
+        self.tol = tol
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+
+        #persist frequent labels in a dictionary
+        self.encoder_dict_ = dict()
+
+        for var in self.variables:
+            #The encoder learns the most frequent categories
+            t = pd.Series(X[var].value_counts())/len(X)
+            #frequent labels
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(X[feature].isin(self.encoder_dict_[feature]), X[feature], 'Rare')
+        return X
