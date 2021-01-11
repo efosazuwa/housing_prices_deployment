5f13929652f7d8520f54499b8de12fa6c2fb2476
Efosa Osazuwa <efosazuwa0@gmail.com>
2021-01-11 15:02:42 -0700

    Adds categorical encoder class

--- preprocesspipeline.py
+++ preprocesspipeline.py
@@ -92,3 +92,37 @@ class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
         for feature in self.variables:
             X[feature] = np.where(X[feature].isin(self.encoder_dict_[feature]), X[feature], 'Rare')
         return X
+
+#string to numbers categorical encoder
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y):
+        temp = pd.concat([X, y], axis=1)
+        temp.columns =  list(X.columns) + ['target']
+
+        self.encoder_dict_
+
+        for var in self.variables:
+            t = temp.groupby(var)['target'].mean().sort_values(ascending=True).index
+            self.encoder_dict_[var] = {k: i  for i, k in enumerate(t,0)}
+        return self
+
+    def transform(self, X):
+        #encode labels
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
+
+        #Check if transformer introduces any NaN
+        if X[self.variables].isnul().any().any():
+            null_counts = X[self.variables].isnull().any()
+            vars_ = {key: value for key, value in null_counts.items() if value
+                     is True}
+            raise errors.InvalidModelInputError("Categorical encoder introduced Nan when transforming categorical variables: {}".format(vars.keys()))
+        return X
