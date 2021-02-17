62cb047e7df65ae4011b5a0d800562d0b6f48826
Efosa Osazuwa <efosazuwa0@gmail.com>
2021-02-16 19:59:02 -0700

    restructure to single function script of training model

--- packages/regression_model/train_pipeline.py
+++ packages/regression_model/train_pipeline.py
@@ -1,25 +1,22 @@
-import pathlib
-
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.externals import joblib
 
-import pipeline
+from regression_model import pipeline
+from regregresion_model.processing.data_management import load_dataset, save_pipeline
+from regresion_model.config import config
 
 def run_training() -> None:
     """Train the model"""
 
-    data = pd.read_csv(TRAINING_DATA_FILE)
+    data = load_dataset(file_name=config.TRAINING_DATA_FILE)
 
-    X_train, X_test, y_train, y_test = train_test_split(data[FEATURES],
-                                                        data[TARGET],
+    X_train, X_test, y_train, y_test = train_test_split(data[config.FEATURES],
+                                                        data[config.TARGET],
                                                         test_size=0.1,
                                                         random_state=0)
     y_train = np.log(y_train)
-    y_test = np.log(y_test)
 
-    pipeline.price_pipe.fit(X_train[FEATURES], y_train)
+    pipeline.price_pipe.fit(X_train[config.FEATURES], y_train)
 
     save_pipeline(pipeline_to_persist=pipeline.price_pipe)
 
