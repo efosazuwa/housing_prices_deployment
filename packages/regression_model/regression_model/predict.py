2ad3c71a27a49d4ef786362a9e07f01496508dee
Efosa Osazuwa <efosazuwa0@gmail.com>
2021-02-19 13:36:40 -0700

    adds mandatory keyword to load_pipeline call

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -5,7 +5,7 @@ from regression_model.processing.data_management import load_pipeline
 from regression_model.config import config
 
 pipeline_file_name = 'regression_model.pkl'
-_price_pipe = load_pipeline(pipeline_file_name)
+_price_pipe = load_pipeline(file_name=pipeline_file_name)
 
 def make_prediction(*, input_data) -> dict:
     """Make model prediction with saved model pipeline"""
